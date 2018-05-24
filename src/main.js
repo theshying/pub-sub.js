929c97a89047115212fa5abcce93ab54cf380859
theShy <969153746@qq.com>
2018-05-24 19:48:28 +0800

    add Offline subscriptions  -on_offline

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@
 
 
 function Event(option) {
-    let eventList = new Map()
-    // 订阅函数
+    const {cache} = option;
+    let eventList = new Map();
+    let emitList = new Map();
+    // 在线订阅函数
     function on(type, ...fn){
         let fns = eventList.get(type);
         if (Array.isArray(fns)) {
@@ -12,8 +14,31 @@ function Event(option) {
             eventList.set(type, [...fn])
         }
     }
+    //离线订阅函数
+    function on_offline(type, ...fn) {
+        on(type, ...fn)
+        if (cache) {
+            if (emitList.has(type)) {
+                emitList.get(type).forEach(item => {
+                    [...fn].forEach(f => {
+                        f(item)
+                    })
+                })
+            }
+        } else {
+            console.warn('option.cache is not true')
+        }
+    }
     // 发布函数
     function emit(type, ...msg){
+        // if option.cache === true
+        if (Boolean(cache)) {
+            let currentMsg = [...msg];
+            if (emitList.has(type)){
+                currentMsg.push(emitList.get(type))
+            }
+            emitList.set(type,currentMsg)
+        }
         let fns = eventList.get(type);
         if (Array.isArray(fns)) {
             fns.forEach(fn => {
@@ -33,11 +58,24 @@ function Event(option) {
             }
         }
     }
+    function once(type, ...fn) {
+
+    }
     return {
         on,
         emit,
         off,
+        once,
+        on_offline
     }
 }
 
 exports.default = Event;
+
+const e = new Event({cache: true})
+e.emit('msg', '1111111')
+e.on_offline('msg', function() {
+    console.log(...arguments)
+})
+
+
